0545cd03ae4dc5f4a8f54ea8a54f97a049ea5c61
ivencic <ivencic@gmail.com>
2024-06-23 21:48:03 +0200

    Create TaskCreateSerilizer and method validate_deadline

--- first_app/serializers/task_serializers.py
+++ first_app/serializers/task_serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from datetime import datetime
+
 from first_app.models.task import Task
 
 
@@ -6,3 +9,14 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'status', 'deadline']
+
+
+class TaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+    def validate_deadline(self, value):
+        if value < datetime.now():
+            raise ValidationError('The deadline cannot be in the past.')
+        return value
